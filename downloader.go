651115d5328dd10113877f79f5221585d0260c2a
Mohamed Hesham <mohamedhesham1619@gmail.com>
2024-09-07 20:09:00 +0300

    download video with hardcoded url

---
+++ downloader.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	
+	videoURL := "https://www.facebook.com/reel/8176615212451714"
+
+	cmd := exec.Command("./yt-dlp", "-o", "H:\\قرآن\\%(title)s.%(ext)s", videoURL)
+
+	// Set the output to the terminal for progress and errors
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	// Run the command
+	fmt.Println("Downloading video:", videoURL)
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println("Error downloading video:", err)
+		return
+	}
+
+	fmt.Println("Download complete.")
+}
