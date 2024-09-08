13e3f9497532b7e985cd9b3fc363f73826bf9d2c
Mohamed Hesham <mohamedhesham1619@gmail.com>
2024-09-08 17:04:50 +0300

    feat: add 'extractUrls' function to extract urls from a text file into a slice of strings

--- downloader.go
+++ downloader.go
@@ -4,16 +4,43 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"slices"
+	"strings"
 )
 
 func main() {
-	
-	videoURL := "https://www.facebook.com/reel/8176615212451714"
 
-	downloadVideo(videoURL)
+	//videoURL := "https://www.facebook.com/reel/8176615212451714"
+
+	//downloadVideo(videoURL)
+	urls, err := extractUrls("urls.txt")
+
+	if err != nil {
+		fmt.Println("couldn't extract urls from the file")
+		return
+	}
+	fmt.Println(urls)
 }
 
-func downloadVideo(videoURL string){
+// extract urls from a text file into a slice of strings
+func extractUrls(fileName string) ([]string, error){
+	content, err := os.ReadFile(fileName)
+
+	if err != nil {
+		return []string{}, err
+	}
+
+	// split the content of the file into a slice using newline as separator
+	urls := strings.Split(string(content), "\r\n")
+	
+	// remove the empty values from the slice
+	urls = slices.DeleteFunc(urls, func(value string) bool {return value == ""})
+
+	return urls, nil
+	
+}
+
+func downloadVideo(videoURL string) {
 	cmd := exec.Command("./yt-dlp", "-o", "H:\\قرآن\\%(title)s.%(ext)s", videoURL)
 
 	// Set the output to the terminal for progress and errors
