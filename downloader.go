d6343ac292d25e40cecdda2a06b144d89d1d14a2
Mohamed Hesham <mohamedhesham1619@gmail.com>
2025-01-22 19:36:27 +0200

    add path flag to set the download directory

--- downloader.go
+++ downloader.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"os/exec"
@@ -9,19 +10,24 @@ import (
 	"sync"
 )
 
+// if the user provides a path flag, the downloaded videos will be saved in that directory. Otherwise, they will be saved in the current directory.
+var pathFlage = flag.String("path", "", "path to the download directory")
+
 func main() {
+	flag.Parse()
+
 	urls, err := extractUrls("urls.txt")
 
 	if err != nil {
 		fmt.Println("couldn't extract urls from the file")
 		return
 	}
-	
+
 	wg := sync.WaitGroup{}
 	wg.Add(len(urls))
 
 	// start downloading videos concurrently
-	for i := 0; i < len(urls); i++{
+	for i := 0; i < len(urls); i++ {
 		go func() {
 			downloadVideo(urls[i])
 			wg.Done()
@@ -33,7 +39,7 @@ func main() {
 }
 
 // extract urls from a text file into a slice of strings
-func extractUrls(fileName string) ([]string, error){
+func extractUrls(fileName string) ([]string, error) {
 	content, err := os.ReadFile(fileName)
 
 	if err != nil {
@@ -42,16 +48,23 @@ func extractUrls(fileName string) ([]string, error){
 
 	// split the content of the file into a slice using newline as separator
 	urls := strings.Split(string(content), "\r\n")
-	
+
 	// remove the empty values from the slice
-	urls = slices.DeleteFunc(urls, func(value string) bool {return value == ""})
+	urls = slices.DeleteFunc(urls, func(value string) bool { return value == "" })
 
 	return urls, nil
-	
+
 }
 
 func downloadVideo(videoURL string) {
-	cmd := exec.Command("./yt-dlp", "-o", "%(title)s.%(ext)s", videoURL)
+
+	downloadPath := "%(title)s.%(ext)s"
+	
+	if *pathFlage != "" {
+		downloadPath = strings.ReplaceAll(*pathFlage, `\`, "/") + "/" + downloadPath
+	}
+	
+	cmd := exec.Command("./yt-dlp", "-o", downloadPath, videoURL)
 
 	// Set the output to the terminal for progress and errors
 	cmd.Stdout = os.Stdout
